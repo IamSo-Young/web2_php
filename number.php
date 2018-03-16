4b62d5796eb83362a668a475795be315a1acff2c
IamSo-Young <35857593+IamSo-Young@users.noreply.github.com>
2018-03-16 18:47:23 +0900

    180307_180309_php
    180307_180309_php 
    number, string, variable

---
+++ number.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+<body>
+<?php
+echo 1;
+print(1);
+echo 1+1;
+ ?>
+<h1>Number & Arithmetic Operator</h1>
+  <h2>1+1</h2>
+  <?php
+  echo 1+1;
+  ?>
+  <h2>2-1</h2>
+  <?php
+  echo 2-1;
+   ?>
+   <h2>2*2</h2>
+   <?php
+   echo 2*2;
+    ?>
+    <h2>4/2</h2>
+    <?php
+    echo 4/2;
+     ?>
+
+</body>
+</html>
---
+++ variable.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <body>
+    <h1>Variable</h1>
+    <?php
+    $a = 10;
+    echo $a+1;
+     ?>
+     <?php
+     $name = "So-Young";
+     echo "Lorem ipsum dolor sit amet,
+     consectetur ".$name." adipisicing elit,
+     sed do eiusmod tempor incididunt ut
+     labore et dolore magna aliqua. Ut enim
+     ad minim veniam, quis nostrud
+     exercitation ullamco ".$name." laboris
+     nisi ut aliquip ex ea
+     commodo consequat. Duis aute irure
+     dolor in reprehenderit in voluptate
+     velit esse cillum dolore ".$name." eu fugiat
+     nulla pariatur. Excepteur sint
+     occaecat cupidatat non proident, sunt
+     in culpa qui officia deserunt mollit
+     anim id est laborum. by ".$name;
+      ?>
+
+  </body>
+</html>
