e290c13685e39ead1ffbc02a1926179c34b25f7b
IamSo-Young <35857593+IamSo-Young@users.noreply.github.com>
2018-04-13 18:24:30 +0900

    add PHP parameter, parameter_url, function, boolean_comparison, conditional,...
    add PHP parameter, parameter_url, function, boolean_comparison, conditional,...

---
+++ comparison.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Comparison Operators &amp; Boolean data type </h1>
+
+    <?php
+    /*echo
+    print
+    */
+    var_dump(1);
+    var_dump('1');
+    var_dump(1+1);
+    var_dump(1==1);
+    var_dump(1==2);
+ ?>
+
+    <h2>1==1</h2>
+    <?php
+    var_dump(1==1);
+     ?>
+     <h2>1>1</h2>
+     <?php
+     var_dump(1>1);
+      ?>
+     <h2>1>=1</h2>
+     <?php
+     var_dump(1>=1);
+      ?>
+  </body>
+</html>
--- index.php
+++ index.php
@@ -1,8 +1,47 @@
 <!doctype html>
 <html>
+  <head>
+    <meta charset="utf-8">
+    <title>가지고 있는 콘솔 게임</title>
+  </head>
   <body>
-    <?php
+    <!--\<\?php
       echo date('Y-m-d H:i:s');
-     ?>
+     ?>-->
+     <h1><a href="index.php">콘솔 게임</a></h1>
+     <ol>
+       <li><a href="index.php?id=MONSTERHUNTER_WORLD">몬스터헌터 월드</a></li>
+       <li><a href="index.php?id=THELEGENDOFZELDA_BREATHOFTHEWILD">젤다의 전설 브레스 오브 더 와일드</a></li>
+       <li><a href="index.php?id=SUPERMARIO_ODYSSEY">슈퍼 마리오 오딧세이</a></li>
+       <li><a href="index.php?id=MARIOKART_8_DELUXE">마리오 카트8 디럭스</a></li>
+       <li><a href="index.php?id=DRAGONBALL_FighterZ">드래곤볼 파이터즈</a></li>
+       <li><a href="index.php?id=THEWITCHER_WILDHUNT_GOTYEDITION">위쳐 3 와일드 헌트 GOTY 에디션</a></li>
+       <li><a href="index.php?id=HORIZONZERODAWN_COMPLETEEDITION">호라이즌 제로 던 컴플리트 에디션</a></li>
+     </ol>
+     <h2>
+       <?php
+       /*'만약에 id값이 있다면*/
+       if(isset($_GET['id']))
+       {
+         echo $_GET['id'];
+       }
+       /*없다면*/
+        else
+        {
+         echo "Welcome";
+        }
+       ?>
+     </h2>
+     <?php
+     if(isset($_GET['id']))
+     {
+       /*echo data/id 값에 해당하는 파일의 내용;*/
+       echo file_get_contents("data/".$_GET['id']);
+     }
+     else
+      {
+        echo "Hello, PHP";
+     }
+      ?>
   </body>
 </html>
